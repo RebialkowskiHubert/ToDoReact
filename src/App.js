7078afb7bba501d23c1baa1e94f4c659b44dd7f0
hubert <hubertrebialkowski@gmail.com>
2019-11-17 17:40:35 +0100

    Added routing and edit form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import ToDoList from './containers/ToDoList/index';
+import ToDoEditForm from './components/ToDoEditForm/index';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
     render() {
-    return (
-      <div>
-        <ToDoList/>
-      </div>
+      return (
+        <Router>
+          <div>
+            <Route exact path='/' component={ToDoList} />
+            <Route path='/todo_items/:item_id' component={ToDoEditForm} />
+          </div>
+        </Router>
     )
   }
 }
