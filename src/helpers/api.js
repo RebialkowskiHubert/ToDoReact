c6eeeeeba968c55550faa5b7c4036c28fb125ade
hubert <hubertrebialkowski@gmail.com>
2019-11-17 12:24:54 +0100

    Added post method to api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -6,3 +6,26 @@ export const get = url =>
                 .then(json => resolve(json));
         }
     );
+
+export const post = (url, body) => 
+    new Promise(
+        (resolve, reject) => apiCall(url, 'POST', body, resolve, reject)
+    );
+
+    
+const apiCall = (url, method, body, resolve, reject) => {
+    fetch(url, {
+        methdod: method,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify(body)
+    }).then(resp => {
+        if(resp.ok) {
+            resp.json().then(json => resolve(json));
+        }
+        else {
+            reject(resp);
+        }
+    });
+}
